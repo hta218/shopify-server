b58c7404cd259466c527bbb2815075bccf5f78e6
Huynh Tuan Anh <huynhtuananh21895@gmail.com>
2018-08-30 11:55:58 +0700

    Remove unnecessary comment

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ const route = require('./routes');
 app.use(bodyParser.json());
 app.use(cors());
 
-
-// Fix the CORS bugs
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
 app.get('/', (req, res) => {
   res.status(200).json({message: "Test request successfully"});
 });
